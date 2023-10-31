b8f9e6ee7356b0f1a44e9677737d0b8cd6be65ad
Andries de Kock <andries.de@gmail.com>
2023-10-31 18:58:08 +0200

    Removed unnecessary class

--- Script/script.js
+++ Script/script.js
@@ -1,12 +1,18 @@
 'use strict';
 
-const setDisplayMessage = function (control, value) {
-	document.querySelector(`${control}`).textContent = value;
+const calculateSecretNumber = function () {
+	return Math.trunc(Math.random() * 20) + 1;
 };
 
-const setPlayerScore = function (score) {
-	document.querySelector('.score').textContent = score;
-};
+const messageObject = document.querySelector('.message');
+const numberObject = document.querySelector('.number');
+const highScoreObject = document.querySelector('.highscore');
+const guessObject = document.querySelector('.guess');
+const scoreObject = document.querySelector('.score');
+
+let score = 20;
+let highScore = 0;
+let secretNumber = calculateSecretNumber();
 
 const setGameBackgroundColor = function (value) {
 	document.querySelector('body').style.backgroundColor = value;
@@ -16,53 +22,46 @@ const setNumberControlWidth = function (value) {
 	document.querySelector('.number').style.width = value;
 };
 
-const Process = function (userInput) {
+const process = function () {
+	let userInput = Number(guessObject.value);
 	if (!userInput) {
 		// no guess
-		setDisplayMessage('.message', '⛔ No Number!');
+		messageObject.textContent = '⛔ No Number!';
 	} else if (userInput === secretNumber) {
 		// guess was correct!
-		setDisplayMessage('.message', '🎉 Correct Number!');
-		setDisplayMessage('.number', secretNumber);
+		messageObject.textContent = '🎉 Correct Number!';
+		numberObject.textContent = secretNumber;
 		// set the player's highscore
-		calculatePlayerScore();
-		setDisplayMessage('.highscore', hightScore);
+		if (score > highScore) highScore = score;
+		highScoreObject.textContent = highScore;
 		// display to user they won
 		setGameBackgroundColor('#60b347');
 		setNumberControlWidth('30rem');
 	} else if (userInput !== secretNumber) {
 		// guess was wrong!
 		if (score > 1) {
-			setDisplayMessage(
-				'.message',
-				userInput > secretNumber ? '📈 Too high!' : '📉 Too low!'
-			);
+			messageObject.textContent = userInput > secretNumber ? '📈 Too high!' : '📉 Too low!';
 			// subtract score for guessing incorrect number
-			subtractPlayerScore();
-			setPlayerScore(score);
+			score--;
+			scoreObject.textContent = score;
 		} else {
 			// player reached 0 score, game over!
-			setDisplayMessage('.message', '💥 You lost the game!');
-			setPlayerScore(0);
+			messageObject.textContent = '💥 You lost the game!';
+			scoreObject.textContent = 0;
 		}
 	}
 };
 
-const ResetGame = function () {
+const reset = function () {
 	score = 20;
-	secretNumber = generateSecretNumber();
-	setPlayerScore(score);
-	setDisplayMessage('.message', 'Start guessing...');
-	setDisplayMessage('.number', '?');
+	secretNumber = calculateSecretNumber();
+	scoreObject.textContent = score;
+	messageObject.textContent = 'Start guessing...';
+	numberObject.textContent = '?';
+	guessObject.value = '';
 	setGameBackgroundColor('#222');
 	setNumberControlWidth('15rem');
-	document.querySelector('.guess').value = '';
 };
 
-document.querySelector('.check').addEventListener('click', function () {
-	Process(Number(document.querySelector('.guess').value));
-});
-
-document.querySelector('.again').addEventListener('click', function () {
-	ResetGame();
-});
+document.querySelector('.check').addEventListener('click', process);
+document.querySelector('.again').addEventListener('click', reset);
